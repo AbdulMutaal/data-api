d673f7858c282d8b6b67b1e931bfbf9f8bc3bf47
Abdul Mutaal Azhar <abdulmutaalazhar@gmail.com>
2022-12-15 16:05:51 +0500

    fixes

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const rateLimit = require('express-rate-limit');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
+        "ssl": {
+            "require": true,
+            "rejectUnauthorized": false
         }
     }
 });
